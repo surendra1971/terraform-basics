6856dea7e39749b295102f0ee9e5c021d6bc6944
Your Name <you@example.com>
2023-07-17 22:52:40 +0530

    updating the Auto Push

--- 05-remote-backend/ec2.tf
+++ 05-remote-backend/ec2.tf
@@ -1,36 +1,8 @@
-# Creates EC2
-resource "aws_instance" "app_server" {
-  ami                     = "ami-0c1d144c8fdd8d690"
-  instance_type           = "t3.nano"
-  vpc_security_group_ids  = [aws_security_group.allow_all.id]
+resource "aws_instance" "remote-app" {
+  ami           = "ami-0c1d144c8fdd8d690"
+  instance_type = "t3.nano"
 
   tags = {
-    Name = "TerraformServer"
-  }
-}
-
-
-# Creates a Security Group
-resource "aws_security_group" "allow_all" {
-  name        = "allow_tls"
-  description = "Allow TLS inbound traffic"
-
-  ingress {
-    description = "TLS from VPC"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "allow_tls"
+    Name = "Linus_Server_With_RemoteBackend"
   }
 }
