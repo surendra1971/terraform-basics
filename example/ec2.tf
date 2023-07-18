945934320d81e8bfbfe89a384deda3251296ddde
Your Name <you@example.com>
2023-07-18 20:22:32 +0530

    updating the Auto Push

--- example/ec2.tf
+++ example/ec2.tf
@@ -1,5 +1,5 @@
 # Creates EC2
-resource "aws_instance" "app_server" {
+resource "aws_instance" "server" {
   ami                     = "ami-0c1d144c8fdd8d690"
   instance_type           = "t3.nano"
   vpc_security_group_ids  = [aws_security_group.allow_all.id]
