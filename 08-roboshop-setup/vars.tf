8e609b983b7cc54e582c7a18a938d3293a86a9a2
Your Name <you@example.com>
2023-07-20 19:51:05 +0530

    updating the Auto Push

--- 08-roboshop-setup/vars.tf
+++ 08-roboshop-setup/vars.tf
@@ -6,22 +6,22 @@
             APP_VERSION = "null"
         },
         catalogue = {
-            APP_VERSION = "null"
+            APP_VERSION = "0.0.8"
         },
         redis = {
             APP_VERSION = "null"
         },
         user = {
-            APP_VERSION = "null"
+            APP_VERSION = "0.0.5"
         },
         cart = {
-            APP_VERSION = "0.0.3"
+            APP_VERSION = "0.0.2"
         },
         mysql = {
             APP_VERSION = "null"
         },
         shipping = {
-            APP_VERSION = "null"
+            APP_VERSION = "0.0.4"
         },
         rabbitmq = {
             APP_VERSION = "null"
@@ -30,7 +30,7 @@
             APP_VERSION = "0.0.3"
         },
         frontend = {
-            APP_VERSION = "null"
+            APP_VERSION = "0.0.3"
         }
     }
  }
